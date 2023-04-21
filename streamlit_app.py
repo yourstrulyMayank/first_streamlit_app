ea4b1969347e3f4e2ca8e293d1d97ed92045fec1
Mayank Harlalka <35811255+yourstrulyMayank@users.noreply.github.com>
2023-04-21 11:45:44 +0530

    Update streamlit_app.py
    Corrected a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-stremlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
